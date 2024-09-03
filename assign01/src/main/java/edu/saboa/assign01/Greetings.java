3e420a2e3e93fbf5c71dc2b524d32068ded7c16d
Alaina Sabo <saboa@sunypoly.edu>
2024-09-03 14:25:32 -0400

    Added a comment

--- assign01/src/main/java/edu/saboa/assign01/Greetings.java
+++ assign01/src/main/java/edu/saboa/assign01/Greetings.java
@@ -2,6 +2,6 @@ package edu.saboa.assign01;
 
 public class Greetings {
     public static void main(String [] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!  Haha hey I did it. I said the thing");
     }
 }
