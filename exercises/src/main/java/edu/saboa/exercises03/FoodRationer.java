15f668728883248ae332a2a2178037856be35dca
Alaina Sabo <saboa@sunypoly.edu>
2024-09-10 21:16:46 -0400

    Assign 1

--- exercises/src/main/java/edu/saboa/exercises03/FoodRationer.java
+++ exercises/src/main/java/edu/saboa/exercises03/FoodRationer.java
@@ -1,4 +1,9 @@
 package edu.saboa.exercises03;
 
 public class FoodRationer {
+    //get # of people, food per day, and total food
+
+    //
+
+
 }
--- exercises/src/main/java/edu/saboa/exercises03/SimpleOregon.java
+++ exercises/src/main/java/edu/saboa/exercises03/SimpleOregon.java
@@ -3,5 +3,14 @@ package edu.saboa.exercises03;
 public class SimpleOregon {
     public static void main(String [] args){
         System.out.println("WELCOME!");
+        Scanner input = new Scanner(System.in);
+
+        double totalFood = input.nexDouble();
+        int numPeople = input.nextInt();
+        int ration = input.nextInt();
+
+        int totalDays = (int)totalFood/(numPeople*ration);
+
+        System.out.println("TOTAL DAYS: " + totalDays);
     }
 }
