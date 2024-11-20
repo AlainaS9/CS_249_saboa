0210dcbea00e76777b3a1db1e8c46f8c00b625b6
Alaina Sabo <saboa@sunypoly.edu>
2024-11-20 02:58:26 -0500

    assignment 05

--- exercises/src/main/java/edu/saboa/exercises15/strategy/Bear.java
+++ exercises/src/main/java/edu/saboa/exercises15/strategy/Bear.java
@@ -3,9 +3,9 @@ package edu.saboa.exercises15.strategy;
 public abstract class Bear {
     protected Moveable moveBehavior;
 
-    public Bear(Moveable m) {
+ //   public Bear(Moveable m) {
 
-    }
+  //  }
 
     public void performMove() {
         moveBehavior.move();
